7bc7e49120b1902e36a99d1b25aa00d04dc7f0eb
Rick Hewitt <Rick.Lira.Morgan@gmail.com>
2017-04-26 19:45:42 -0700

    Update prime_generator.c with sieve of sundaram.

--- prime_generator.c
+++ prime_generator.c
@@ -1,28 +1,33 @@
 #include <stdio.h>
+#include <math.h>
 
-void sundaram_sieve(int lower_bound, int upper_bound)
+void sundaram_sieve(long long lower_bound, long long upper_bound)
 {
-	int n = upper_bound/2 - 1;
-	int numbers[n];
+	unsigned long long n = ceil( (upper_bound-1)/2 );
+	unsigned long long numbers[n];
 
-	for(int i = 0; i < n; i++) //List of composites
-		numbers[i] = i;	
-	
-	for(int j = 1; j <= n/3 + 1; j++)
+	for(unsigned long long i = 0; i < n; i++) //List of composites
+		numbers[i] = i+1;	
+
+	for(unsigned long long j = 1; j <= n/3 + 1; j++)
 	{
-		for(int i = 1; i <= j; i++)
+		for(unsigned long long i = 1; i <= j; i++)
 		{
-			numbers[i+j+2*i*j] = 0;
+			if(i+j+2*i*j <= n)
+				numbers[i+j+2*i*j - 1] = 0;
 		}
 	}
 
+	if(2 >= lower_bound && 2 <= upper_bound)
+		printf("2\n"); //Prints 2 since sieve only finds odd primes
+
 	//Double all numbers and increment by one giving primes under upper_bound found
-	for(int i = 0; i < n; i++)
+	for(unsigned long long i = 0; i < n; i++)
 	{
-		if(numbers[i] != 0)
+		if(numbers[i] != 0 && numbers[i]*2 + 1 > lower_bound)
 		{
 			numbers[i] = numbers[i]*2 + 1;
-			printf("%d", numbers[i]);
+			printf("%llu\n", numbers[i]);
 		}
 	}
 }
@@ -31,13 +36,14 @@ int main()
 {
 	int test_cases;
 	scanf("%d", &test_cases);
-	int lower_bound = 0;
-	int upper_bound = 0;
-
+	unsigned long long lower_bound = 0;
+	unsigned long long upper_bound = 0;
 	for(int i = 1; i <= test_cases; i++) //Gather boundaries m < n	
 	{
-		scanf("%d %d", &lower_bound, &upper_bound);
+		scanf("%llu %llu", &lower_bound, &upper_bound);
 		sundaram_sieve(lower_bound, upper_bound);
 		printf("\n");
 	}
+
+	return 0;
 }
